9a4d1f7e9b790d170ce214f748a3eff4e9a8847e
Guillaume de Kermadec <ghdekermadec@gmail.com>
2025-06-06 08:39:16 +0000

    modif table renta par commune

--- models/intermediate/renta_par_commune.sql
+++ models/intermediate/renta_par_commune.sql
@@ -5,6 +5,8 @@ SELECT
     px.median_m2,
     re.intensite_tension_immo,
     re.rental_med_all,
+    re.city_name_normalized,
+    re.coordonnees,
     ROUND(SAFE_DIVIDE(re.rental_med_all*12, px.median_m2),2)*100 AS renta_brute
 FROM {{ ref('prix_m²_par_an_par_commune') }} AS px
 JOIN {{ ref('real_estate_info_by_municipality_dpt') }} AS re
