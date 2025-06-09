54f316650005676e4f7501e61d418c6f2af01a31
Samuel Velty <samuelvelty89@gmail.com>
2025-06-09 09:36:21 +0000

    correction du naming tourism_area

--- models/mart/nb_site_touristique_by_department.sql
+++ models/mart/nb_site_touristique_by_department.sql
@@ -3,7 +3,11 @@ geo.department_code,
 geo.department_name,
 site.municipality_code,
 site.typologie_site_touristique,
-site.tourism_area,
+CASE
+  WHEN site.tourism_area = "1" THEN "world heritage"
+  WHEn site.tourism_area = "2" THEN "historical monuments"
+  ELSE site.tourism_area
+END AS tourism_area,
 site.name_site,
 site.coordonnees_activites,
 site.score_site_pondere AS score_site_touristique
@@ -12,3 +16,4 @@ site.score_site_pondere AS score_site_touristique
 FROM {{ ref('int_POI_join') }} AS site
 LEFT JOIN {{ ref('int_geo_ref_clean') }} AS geo
 ON geo.municipality_code = site.municipality_code
+
